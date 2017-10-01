1ed53a917c7eaff392d10d9598dfedef8914173c
Brendan B <n>
2017-10-01 15:31:31 -0700

    added main page and main page. Updaed CSS and Javascript

---
+++ WebContent/resources/js/scripts.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($){
+	$("#login-section").mouseover(function(){
+		$(".register-form").hide();
+		$(this).animate({backgroundColor: "#ff0000" });
+		$(".login-form").slideDown(1000, "swing").delay(1000);
+	});
+	
+	$("#login-section").mouseout(function(){
+		$(this).animate({backgroundColor: "#000000" });
+	});
+	
+	
+	$("#register-section").mouseover(function(){
+		$(".login-form").hide();
+		$(this).animate({backgroundColor: "#ff0000" });
+		$(".register-form").slideDown(1000, "swing").delay(1000);
+	});
+	
+	$("#register-section").mouseout(function(){
+		$(this).animate({backgroundColor: "#000000" });
+	});
+	
+	$(".register-form").mouseover(function(){
+		$(".login-form").hide();
+	});
+	
+	$(".login-form").mouseover(function(){
+		$(".register-form").hide();
+	});
+});
--- src/Beans/User.java
+++ src/Beans/User.java
@@ -9,11 +9,15 @@ public class User
 {
 	private String firstName="";
 	private String lastName="";
+	private String email="";
+	private String phone="";
+	private String username="";
+	private String password="";
 	
 	public User() 
 	{
-		this.firstName = "Brendan";
-		this.lastName = "Brooks";
+		this.firstName = "";
+		this.lastName = "";
 	}
 
 	public String getFirstName() 
@@ -35,5 +39,39 @@ public class User
 	{
 		this.lastName = lastName;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 
 }
